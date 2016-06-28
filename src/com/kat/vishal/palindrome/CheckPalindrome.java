72ed5ec31bebe3a5aabf77c12aa9ba2963d56d4b
VISHAL <VISHAL@DELL>
2016-06-28 18:08:49 +0530

    final change

--- src/com/kat/vishal/palindrome/CheckPalindrome.java
+++ src/com/kat/vishal/palindrome/CheckPalindrome.java
@@ -4,7 +4,14 @@ public class CheckPalindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PalindromeChecker pc = new PalindromeChecker();
+		
+		if(pc.isPalindrome(123454321)){
+			System.out.println("the integer is palindrome");
+		}
+		if(pc.isPalindrome("aabbccbbaa")){
+			System.out.println("the string is palindrome");
+		}
 	}
 
 }
--- src/com/kat/vishal/palindrome/PalindromeChecker.java
+++ src/com/kat/vishal/palindrome/PalindromeChecker.java
@@ -1,5 +1,42 @@
 package com.kat.vishal.palindrome;
 
-public class PalindromeChecker {
+public class PalindromeChecker implements CanCheckPalindrome{
 
+	@Override
+	public boolean isPalindrome(String s) {
+		// TODO Auto-generated method stub
+		StringBuffer str = new StringBuffer();
+		StringBuffer str2 = new StringBuffer();
+		str.append(s);
+		str2 = str.reverse();
+		
+		if(str2 == str){
+			return true;
+		}
+		else{
+		return false;
+		}
+	}
+
+	@Override
+	public boolean isPalindrome(Integer i) {
+		// TODO Auto-generated method stub
+		int temp = i;
+		int rev = 0;
+		int rem =0;
+		
+		while(temp != 1){
+		rem = temp % 10;
+		rev = (rev*10) + rem;
+		temp = temp / 10;
+		}
+		
+		if(rev == i){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
 }
